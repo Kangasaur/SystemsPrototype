c55d5876b61bccf80b6a31367dfb4f5f67e7cb8f
Seth <szr230@nyu.edu>
2021-11-15 18:11:15 -0500

    add wire and branch physics

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -6,6 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed;
     public float jumpSpeed;
+    bool onGround = true;
     float hmove;
     Rigidbody2D rb;
 
@@ -28,9 +29,15 @@ public class PlayerMove : MonoBehaviour
     }
     void CheckJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            onGround = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onGround = true;
     }
 }
